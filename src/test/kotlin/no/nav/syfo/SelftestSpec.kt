7c3622a263cb7a7b5f34f62f742a6a4c3205f417
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2025-07-12 07:15:47 +0200

    Update SelftestSpec.kt

--- src/test/kotlin/no/nav/syfo/SelftestSpec.kt
+++ src/test/kotlin/no/nav/syfo/SelftestSpec.kt
@@ -11,22 +11,26 @@ import org.amshove.kluent.shouldBeEqualTo
 class SelftestSpec :
     FunSpec({
         context("Successfull liveness and readyness tests") {
-            testApplication {
-                start()
-                application.configureInternalRouting()
 
                 test("Returns ok on is_alive") {
-                    with(handleRequest(HttpMethod.Get, "/internal/is_alive")) {
+                    testApplication {
+                    application {
+                        configureInternalRouting()
+                    }
+                    val response = client.get("/internal/is_alive")
                         response.status() shouldBeEqualTo HttpStatusCode.OK
                         response.content shouldBeEqualTo "I'm alive! :)"
                     }
                 }
                 test("Returns ok in is_ready") {
-                    with(handleRequest(HttpMethod.Get, "/internal/is_ready")) {
+                    testApplication {
+                        application {
+                        configureInternalRouting()
+                        }
+                        val response = client.get("/internal/is_ready")
                         response.status() shouldBeEqualTo HttpStatusCode.OK
                         response.content shouldBeEqualTo "I'm ready! :)"
                     }
                 }
             }
-        }
     })
